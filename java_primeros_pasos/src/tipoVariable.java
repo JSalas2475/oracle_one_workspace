5bd641c9ef2fe0204a61ddadcfe9d19596a3ef48
Jorge Salas <jorge.a.salas.chavez@gmail.com>
2023-07-06 23:01:00 -0600

    javajul6last

--- java_primeros_pasos/src/tipoVariable.java
+++ java_primeros_pasos/src/tipoVariable.java
@@ -1,12 +1,18 @@
 public class tipoVariable {
 
     public static void main(String [] args){
+        double variable1 = 230.89;
+        int variable1Entero = (int) variable1; // Cast
+        //Este método imprime en la consola
+        System.out.println(variable1Entero);
+        //Otros tipos de numeros
+        long prueba = 1222222222222222222L;
+        short numeroPequeno = 13555;
+        byte numeroDiminuto = 123;
+        float numeroDecimalPequeno = 2.5F;
 
-        System.out.println("Hola nuevo test");
-
-        int edad = 37;
-
-        System.out.println("La edad es "+ edad + ", Felicitaciones!");
+        double resultado = variable1Entero + variable1;
+        System.out.println(resultado);
     }
 
 }
