6fe7258a1276dffe3f5795ecd658648ac3343519
Jorge Salas <jorge.a.salas.chavez@gmail.com>
2023-07-21 18:56:52 -0600

    polimorfismo bytebank

---
+++ bytebank-heredado/src/TestControlBonificacion.java
@@ -0,0 +1,20 @@
+public class TestControlBonificacion {
+    public static void main(String[] args) {
+        Funcionario jorge = new Funcionario();
+        jorge.setSalario(2000);
+
+        Gerente Valeria = new Gerente();
+        Valeria.setSalario(6000);
+
+        Contador carlos = new Contador();
+        carlos.setSalario(4000);
+
+
+        ControlBonificacion controlBonificacion = new ControlBonificacion();
+
+        controlBonificacion.registrarSalario(jorge);
+        controlBonificacion.registrarSalario(Valeria);
+        controlBonificacion.registrarSalario(carlos);
+
+    }
+}
